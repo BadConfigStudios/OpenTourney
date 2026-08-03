6a1825fa6b611c9a83093218b2f8e1bfdae4bb8b
GenNetEng <bkcrisler@gmail.com>
2026-08-03 09:32:49 -0500

    docs: link the published OpenAPI spec from the docs site

--- docs/conf.py
+++ docs/conf.py
@@ -7,5 +7,6 @@ extensions = ["sphinx.ext.autodoc"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build"]
+html_extra_path = ["openapi.json"]
 
 html_theme = "alabaster"
--- docs/index.rst
+++ docs/index.rst
@@ -3,5 +3,7 @@ OpenTourney
 
 Game-agnostic, open tournament-tracking standard and engine.
 
+`Download the OpenAPI spec (JSON) <openapi.json>`_
+
 .. toctree::
    :maxdepth: 2
